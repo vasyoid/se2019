b7f0a08df7fbf27e8143ca3f4edc05f6ad35c402
vasyoid <vasyoid@gmail.com>
2019-02-06 05:38:19 +0300

    Finally fix newline problem

--- cli/src/test/kotlin/net/netau/vasyoid/command/EchoTest.kt
+++ cli/src/test/kotlin/net/netau/vasyoid/command/EchoTest.kt
@@ -14,8 +14,8 @@ class EchoTest {
         Assert.assertTrue(Echo(System.`in`.bufferedReader(), arguments, outputStream.bufferedWriter()).run())
         val outputString = String(ByteArrayInputStream(outputStream.toByteArray()).readBytes())
         Assert.assertEquals(
-            arguments.joinToString(" "),
-            outputString.dropLast(1)
+            arguments.joinToString(" ") + System.lineSeparator(),
+            outputString
         )
     }
 }
