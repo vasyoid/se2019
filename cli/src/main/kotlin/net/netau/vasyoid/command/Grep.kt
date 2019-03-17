4bd8fbf148266eb57ae68dc736083b304210ee2e
vasyoid <vasyoid@gmail.com>
2019-03-17 19:22:12 +0300

    Fix grep issues

--- cli/src/main/kotlin/net/netau/vasyoid/command/Grep.kt
+++ cli/src/main/kotlin/net/netau/vasyoid/command/Grep.kt
@@ -2,9 +2,9 @@ package net.netau.vasyoid.command
 
 import joptsimple.OptionException
 import joptsimple.OptionParser
-import java.io.BufferedReader
-import java.io.BufferedWriter
-import java.lang.RuntimeException
+import joptsimple.OptionSet
+import net.netau.vasyoid.exception.CommandException
+import java.io.*
 
 /**
  * Grep command. Prints lines that contain a specified pattern.
@@ -17,17 +17,16 @@ class Grep(
 
     private var lines = 0
     private lateinit var regex: Regex
+    private var grepInput = input
 
     /**
      * @inheritDoc
      */
     override fun run(): Boolean {
-        if (!parseArguments()) {
-            return false
-        }
+        parseArguments()
         var linesToPrint = 0
         while (true) {
-            val line = input.readLine() ?: break
+            val line = grepInput.readLine() ?: break
             if (line.contains(regex)) {
                 linesToPrint = lines
             }
@@ -41,29 +40,42 @@ class Grep(
         return true
     }
 
-    private fun parseArguments(): Boolean {
+    private fun parseArguments() {
         val parser = OptionParser()
         parser.accepts("i", "ignore case")
         parser.accepts("w", "look for full words")
         parser.accepts("A", "print n lines after match")
             .withRequiredArg().describedAs("n").ofType(Integer::class.java)
+        lateinit var options: OptionSet
         try {
-            val options = parser.parse(*arguments.toTypedArray())
-            if (options.has("A")) {
-                lines = options.valueOf("A") as Int
-            }
-            lines++
-            initRegex(options.has("i"), options.has("w"), options.nonOptionArguments().single() as String)
+            options = parser.parse(*arguments.toTypedArray())
         } catch (e: OptionException) {
-            System.err.println(e.message + "\n")
-            parser.printHelpOn(System.out)
-            return false
-        } catch (e: RuntimeException) {
-            System.err.println("Specify exactly one pattern\n")
-            parser.printHelpOn(System.out)
-            return false
+            fail(e.message ?: "", parser)
         }
-        return true
+        if (options.has("A")) {
+            lines = options.valueOf("A") as Int
+            if (lines < 0) {
+                fail("context length must be non-negative", parser)
+            }
+        }
+        lines++
+        val args = options.nonOptionArguments()
+        when {
+            args.size < 1 -> fail("Specify exactly one pattern", parser)
+            args.size > 2 -> fail("Too many arguments", parser)
+            args.size == 2 -> try {
+                grepInput = FileInputStream(File(args[1] as String)).bufferedReader()
+            } catch (e: IOException) {
+                throw CommandException("grep: " + e.message)
+            }
+        }
+        initRegex(options.has("i"), options.has("w"), options.nonOptionArguments()[0] as String)
+    }
+
+    private fun fail(message: String, parser: OptionParser) {
+        val helpWriter = StringWriter()
+        parser.printHelpOn(helpWriter)
+        throw CommandException(message + "\n" + helpWriter.toString())
     }
 
     private fun initRegex(ignoreCase: Boolean, words: Boolean, pattern: String) {
